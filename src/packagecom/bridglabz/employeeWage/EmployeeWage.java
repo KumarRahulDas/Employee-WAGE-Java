cd2210e66495d1f575b545d6c7fba6c20b87f1ea
KumarRahulDas <dasrahulbxrepa7061503554@gmail.com>
2023-01-07 02:28:49 -0800

    Uc3 Commite

--- src/packagecom/bridglabz/employeeWage/EmployeeWage.java
+++ src/packagecom/bridglabz/employeeWage/EmployeeWage.java
@@ -1,5 +1,4 @@
 package com.bridglabz.employeeWage;
-
 import java.util.Random;
 
 public class EmployeeWage {
@@ -7,13 +6,28 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        Random random= new Random();
-        int attendance=random.nextInt(2);
+        Random random = new Random();
+        int attendance = random.nextInt(3);
 
-        if(attendance == 1)
-            System.out.println("Employee Present..");
-        else
-            System.out.println("Employee Absent..");
+        int wagePerHour = 20;
+        int fullDayHours = 8;
+        int partTimeHours = 4;
+        int wagePerDay = 0;
+
+        switch (attendance) {
+            case 0:
+                System.out.println("Employee Absent.");
+                break;
+            case 1:
+                wagePerDay = partTimeHours * wagePerHour;
+                break;
+            default:
+                wagePerDay = fullDayHours * wagePerHour;
+
+        }
+
+        System.out.println("Employee Wage Per Day: " + wagePerDay);
+
     }
 
 }
